79537eda9d159b0de7d7d7378e90d42bcd639220
malakhoeriah <mala123@gmail.com>
2019-02-20 10:23:04 +0700

    episode 2

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+<<<<<<< HEAD
+=======
+Route::prefix('admin')->group(function(){
+	Route::get('/', function(){
+		return view('admin.main');
+	})->name('admin.home');
+});
+
+>>>>>>> episode-2
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
